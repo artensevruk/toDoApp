f74b12b300bf95ae2b7975a1d9a7a0bbce7acf57
Anton <kurguzenkov@gmail.com>
2023-08-18 13:00:42 +0300

    improve webpack build configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,34 @@
 import path from "path";
 import { fileURLToPath } from "url";
 
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
 const file = fileURLToPath(import.meta.url);
-const rootDir = path.dirname(file)
-const config = {
+const rootDir = path.dirname(file);
+
+export default (env, argv) => ({
+  mode: argv.mode,
+  devtool: argv.mode === 'development'
+    ? 'source-map'
+    : false,
+  plugins:  [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({ title: 'ToDoList' })
+  ],
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(`${rootDir}/dist`),
+    filename: 'bundle.js'
+  },
   module: {
     rules: [
       {
-        test: /\.scss$/,
+        test: /\.scss$/i,
         use: [
-          "style-loader",
+          argv.mode !== "production"
+            ? "style-loader"
+            : MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
             options: { sourceMap: true }
@@ -20,18 +39,5 @@ const config = {
         ]
       }
     ]
-  },
-  devServer: {
-    static: `${rootDir}/static`,
-  },
-};
-
-export default (env, argv) => {
-  config.mode = argv.mode;
-
-  if (argv.mode === 'development') {
-    config.devtool = 'source-map';
   }
-
-  return config;
-};
+});
